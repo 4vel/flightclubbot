e3815de10aab01bc815e8d82359676fb5d5de315
Левченко Алексей Сергеевич <Levchenko.A.Sergeevic@sberbank.ru>
2021-01-13 21:04:38 +0300

    bug fixes

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -9,11 +9,14 @@ from utils.misc import rate_limit
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
     text = [
+        """
+         Это бот FlightClubber✈️. Он самостоятельно проверяет цены на авиабилеты сравнивая их
+         с заданными пороговыми значениями. При появлении более низких цен, отправляет сообщение в телеграм """,
         'Список команд: ',
-        '/start - Начать диалог',
+        '/start - Начать диалог и указать аэропорт отправления',
         '/help - Получить справку',
-        '/items - Выбрать итемы',
-        '/form - Заполнить форму',
-        '/menu - Посмотреть меню',
+        '/add_destination - Добавить код аэропорта и цену для поиска билетов',
+        '/show_destinations - Показать список указанных аэропортов и цен',
+        '/remove_destinations - Удалить все направления из списка ',
     ]
     await message.answer('\n'.join(text))
